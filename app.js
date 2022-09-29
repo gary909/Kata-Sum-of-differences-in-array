dc72a642cc4fddd971cdcff92c6189aae9c07368
gary909 <garywhite61@hotmail.com>
2022-09-29 13:32:22 +0200

    code working

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 function sumOfDifferences(arr) {
+    if (arr.length <= 1) {
+        return 0;
+    } else {
+        // let myArr = arr;
+        // myArr.sort(function (b, a) {
+        //     return a - b;
+        // });
+        // let sumA = myArr[0] - myArr[1];
+        // let sumB = myArr[1] - myArr[2];
+        // return sumA + sumB;
+        return Math.abs(Math.min(...arr) - Math.max(...arr));
+    }
 }
 
 console.log(sumOfDifferences([1, 2, 10])); // 9
